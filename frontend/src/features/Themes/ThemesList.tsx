4c682db381f1390cc902f70234c245445da59ea9
AlexandrChaladze <k-team@mail.ru>
2023-06-23 16:30:30 +0300

    css

--- frontend/src/features/Themes/ThemesList.tsx
+++ frontend/src/features/Themes/ThemesList.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
-import ThemesItem from './ThemeItem';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import ThemesItem from "./ThemeItem";
 
 function ThemesList(): JSX.Element {
   const { themes } = useSelector((store: RootState) => store.themeReducer);
-//   console.log(themes);
+  //   console.log(themes);
 
   return (
-    <div>
-        {themes.map((theme) => (
-          <ThemesItem theme={theme} key={theme.id} />
-        ))}
+    <div className="list">
+      {themes.map((theme) => (
+        <ThemesItem theme={theme} key={theme.id} />
+      ))}
     </div>
   );
 }
